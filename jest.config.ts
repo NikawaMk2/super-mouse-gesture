515e9a16dbc3ea93d0588c741b6664d73766ff87
Nikawa <burorisuto3@gmail.com>
2025-03-15 21:43:36 +0900

    chore: jest用の設定に変更

--- jest.config.ts
+++ jest.config.ts
@@ -2,8 +2,16 @@ import type { Config } from 'jest';
 
 const config: Config = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
   testMatch: ['<rootDir>/test/**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleDirectories: ['node_modules', 'src'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  }
 };
 
 export default config;
