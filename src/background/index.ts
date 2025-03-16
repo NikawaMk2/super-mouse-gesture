446772d02c1e9adc82a0302593fe5ffeabbbb08e
Nikawa <burorisuto3@gmail.com>
2025-03-16 16:28:16 +0900

    fix: 拡張機能のアンインストール時に破棄を行うよう修正

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,3 +1,14 @@
+import Logger from "../common/utils/logger";
+import ChromeMessageListener from "./listener/chrome_message_listener";
+
 chrome.runtime.onInstalled.addListener(() => {
-  console.log("Mouse Gesture Extension installed.");
+    Logger.debug('拡張機能インストール');
+});
+
+const chromeMessageListener = new ChromeMessageListener();
+
+// 拡張機能のアンロード時にリスナーを破棄
+chrome.runtime.onSuspend.addListener(() => {
+    chromeMessageListener.dispose();
+    Logger.debug('拡張機能アンロード');
 });
