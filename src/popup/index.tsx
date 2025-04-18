6074015a3a1659ea5ac9a637fc2adf60c4a9119b
Nikawa <burorisuto3@gmail.com>
2025-04-18 22:24:11 +0900

    add: ポップアップページのHTML、TypeScript、Reactコンポーネントを追加し、ポップアップ機能の基盤を構築

---
+++ src/popup/index.tsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client';
+import Popup from './components/component';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(<Popup />);
