deebd7a2b037d9952c7cf1c15d74065fc038a787
Nikawa <burorisuto3@gmail.com>
2025-04-27 15:50:55 +0900

    add: Jestの設定にChrome環境用のセットアップファイルを追加し、テスト環境を整備

---
+++ test/setup/jest.chrome.setup.ts
@@ -0,0 +1,19 @@
+(global as any).chrome = {
+  runtime: {
+    sendMessage: jest.fn((message, callback) => {
+      if (callback) {
+        callback({});
+      }
+    }),
+    onMessage: {
+      addListener: jest.fn(),
+      removeListener: jest.fn(),
+    },
+    lastError: null,
+  },
+  tabs: {
+    create: jest.fn(),
+    query: jest.fn(),
+    update: jest.fn(),
+  },
+}; 
