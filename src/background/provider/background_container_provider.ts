2f6e6f26f0f68562f65efca2030e3c47c495c0e2
Nikawa <burorisuto3@gmail.com>
2025-05-02 23:21:14 +0900

    update: BackgroundContainerProviderクラスにGestureActionHandler、DragActionHandler、MessageListenerをシングルトンでbindする処理を追加。

--- src/background/provider/background_container_provider.ts
+++ src/background/provider/background_container_provider.ts
@@ -1,6 +1,9 @@
 import { Container } from 'inversify';
 import Logger from '../../common/logger/logger';
 import { IContainerProvider } from '../../common/provider/i_container_provider';
+import { MessageListener } from '../services/message_listener';
+import { GestureActionHandler } from '../services/gesture_action_handler';
+import { DragActionHandler } from '../services/drag_action_handler';
 
 export class BackgroundContainerProvider implements IContainerProvider {
     private container: Container | null = null;
@@ -15,7 +18,10 @@ export class BackgroundContainerProvider implements IContainerProvider {
     private initialize(): Container {
         try {
             const container = new Container();
-            // TODO: 必要なサービスクラスをここでbindする
+            // サービスクラスをシングルトンでbind
+            container.bind(GestureActionHandler).toSelf().inSingletonScope();
+            container.bind(DragActionHandler).toSelf().inSingletonScope();
+            container.bind(MessageListener).toSelf().inSingletonScope();
             return container;
         } catch (error) {
             Logger.error('バックグラウンドコンテナ初期化エラー', { error });
