882163e99cb8ae6cf0151811bc33ebcc27016544
Nikawa <burorisuto3@gmail.com>
2025-05-02 22:19:45 +0900

    update: MouseGestureHandlerクラスにジェスチャトレイル描画機能を追加し、アクション通知をリアルタイムで表示する機能を実装。ジェスチャトレイルの開始、更新、クリア処理を強化し、インスタンス破棄メソッドを追加。

--- src/content/handlers/mouse_gesture_handler.ts
+++ src/content/handlers/mouse_gesture_handler.ts
@@ -5,15 +5,23 @@ import { GestureActionType } from '../services/gesture_action/gesture_action_typ
 import { Direction } from '../models/direction';
 import { MouseGestureSettingsService } from '../services/gesture_action/settings/mouse_gesture_settings_service';
 import { DEFAULT_MOUSE_GESTURE_SETTINGS } from '../../common/constants/mouse_gesture_settings';
+import { GestureTrail } from './gesture_trail';
+import { ActionNotification } from './action_notification';
 
 export class MouseGestureHandler {
     private isGesture: boolean = false;
     private gestureTrail: Array<Point> = [];
     private directionTrail: Array<Direction> = [];
     private mouseGestureSettingsService: MouseGestureSettingsService;
+    private gestureTrailRenderer: GestureTrail;
 
     constructor(mouseGestureSettingsService: MouseGestureSettingsService) {
         this.mouseGestureSettingsService = mouseGestureSettingsService;
+        this.gestureTrailRenderer = new GestureTrail({
+            color: 'rgba(255, 0, 0, 0.5)',
+            width: 3,
+            zIndex: 999999
+        });
     }
 
     public onMouseDown(e: MouseEvent) {
@@ -21,7 +29,7 @@ export class MouseGestureHandler {
         this.gestureTrail = [new Point(e.clientX, e.clientY)];
         this.directionTrail = [];
         Logger.debug('マウスジェスチャ開始', { x: e.clientX, y: e.clientY });
-        // TODO: ジェスチャトレイル描画開始（UI表示クラス呼び出し）
+        this.gestureTrailRenderer.startDrawing(new Point(e.clientX, e.clientY));
     }
 
     public onMouseMove(e: MouseEvent) {
@@ -33,23 +41,36 @@ export class MouseGestureHandler {
             this.directionTrail.push(direction);
         }
         this.gestureTrail.push(currentPoint);
-        // TODO: ジェスチャトレイル描画更新（UI表示クラス呼び出し）
+        this.gestureTrailRenderer.updateTrail(currentPoint);
+
+        // アクション名をリアルタイム表示
+        this.analyzeGesturePattern(this.directionTrail).then((pattern) => {
+            if (pattern) {
+                ActionNotification.show(pattern);
+            } else {
+                ActionNotification.hide();
+            }
+        });
     }
 
     public async onMouseUp(e: MouseEvent) {
         if (!this.isGesture) return;
         this.isGesture = false;
-        // TODO: ジェスチャトレイル描画終了・クリア（UI表示クラス呼び出し）
+        this.gestureTrailRenderer.clearTrail();
         const pattern = await this.analyzeGesturePattern(this.directionTrail);
         if (pattern) {
             Logger.info('ジェスチャパターン認識', { pattern });
             try {
                 const action = GestureActionFactory.create(pattern) as { execute: () => void };
                 action.execute();
-                // TODO: アクション通知表示（UI表示クラス呼び出し）
+                // アクション通知UIを非表示
+                ActionNotification.hide();
             } catch (err) {
                 Logger.warn('未対応のジェスチャパターン', { pattern });
+                ActionNotification.hide();
             }
+        } else {
+            ActionNotification.hide();
         }
         this.gestureTrail = [];
         this.directionTrail = [];
@@ -63,6 +84,12 @@ export class MouseGestureHandler {
 
     public isActive(): boolean {
         return this.isGesture;
+    }
+
+    public destroy(): void {
+        this.gestureTrailRenderer.destroy();
+        ActionNotification.destroy();
+        Logger.debug('MouseGestureHandler インスタンス破棄');
     }
 
     // パターン解析: 方向列を文字列化し、設定値からアクション名を返す
