f53386d04331a4ccb7bbc7aa204831789406b50f
Nikawa <burorisuto3@gmail.com>
2025-05-04 21:47:34 +0900

    fix: GestureActionHandlerクラスのhandleメソッドのログレベルをinfoからdebugに変更。

--- src/background/services/gesture_action_handler.ts
+++ src/background/services/gesture_action_handler.ts
@@ -4,7 +4,7 @@ import Logger from '../../common/logger/logger';
 
 export class GestureActionHandler implements IGestureActionHandler {
     async handle(payload: GestureActionMessagePayload): Promise<void> {
-        Logger.info('ジェスチャアクション受信', { payload });
+        Logger.debug('ジェスチャアクション受信', { payload });
         // TODO: アクション名ごとに処理を分岐し、タブ操作等を実装
     }
 } 
