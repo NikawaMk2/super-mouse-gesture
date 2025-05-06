24562ae81fdff10075eda179141610f39ab96d30
Nikawa <burorisuto3@gmail.com>
2025-05-06 21:57:55 +0900

    add: バックグラウンド初期設定時にデバッグログを追加し、初期設定の開始と完了を記録。

--- src/background/index.ts
+++ src/background/index.ts
@@ -2,6 +2,9 @@ import { BackgroundContainerProvider } from './provider/background_container_pro
 import { MessageListener } from './services/message_listener';
 import { GestureActionHandler } from './services/gesture_action_handler';
 import { DragActionHandler } from './services/drag_action_handler';
+import Logger from '../common/logger/logger';
+
+Logger.debug('バックグラウンド初期設定開始');
 
 const containerProvider = new BackgroundContainerProvider();
 const container = containerProvider.getContainer();
@@ -11,3 +14,5 @@ const dragActionHandler = container.get(DragActionHandler);
 const messageListener = new MessageListener(gestureActionHandler, dragActionHandler);
 
 messageListener.listen();
+
+Logger.debug('バックグラウンド初期設定完了');
