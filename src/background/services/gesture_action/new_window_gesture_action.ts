1aae1ebdbd460476e2d690b465aff756d1522584
Nikawa <burorisuto3@gmail.com>
2025-05-06 21:53:09 +0900

    add: NewWindowGestureActionクラスを新規作成し、新しいウィンドウを開く機能を実装。関連するユニットテストも追加。

---
+++ src/background/services/gesture_action/new_window_gesture_action.ts
@@ -0,0 +1,12 @@
+import { IGestureAction } from './gesture_action';
+import { IWindowOperator } from './i_window_operator';
+import Logger from '../../../common/logger/logger';
+
+export class NewWindowGestureAction implements IGestureAction {
+    constructor(private windowOperator: IWindowOperator) {}
+
+    async execute(): Promise<void> {
+        Logger.debug('NewWindowGestureAction: execute() が呼び出されました');
+        await this.windowOperator.createNewWindow();
+    }
+} 
