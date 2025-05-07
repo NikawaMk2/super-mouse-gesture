c4e5feefa2be043e9f60381c5fca8b3381342e78
Nikawa <burorisuto3@gmail.com>
2025-05-07 21:01:40 +0900

    update: OpenAsUrlDragActionクラスに依存性注入を追加し、ITabOperatorの型を明示的に指定。コードの可読性と保守性を向上。

--- src/background/services/drag_action/open_as_url_drag_action.ts
+++ src/background/services/drag_action/open_as_url_drag_action.ts
@@ -1,11 +1,13 @@
 import { DragAction } from './drag_action';
 import { DragActionMessagePayload } from '../../../content/services/message/message_types';
 import Logger from '../../../common/logger/logger';
-import { ITabOperator } from '../../../common/provider/tab_operator';
+import type { ITabOperator } from '../../../common/provider/tab_operator';
+import { injectable, inject } from 'inversify';
 
+@injectable()
 export class OpenAsUrlDragAction implements DragAction {
     private tabOperator: ITabOperator;
-    constructor(tabOperator: ITabOperator) {
+    constructor(@inject('ITabOperator') tabOperator: ITabOperator) {
         this.tabOperator = tabOperator;
     }
     async execute(payload: DragActionMessagePayload): Promise<void> {
