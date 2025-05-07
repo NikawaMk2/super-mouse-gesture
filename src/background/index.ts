3e186ebf62e3b7f19155b524bd81408742318bda
Nikawa <burorisuto3@gmail.com>
2025-05-07 21:03:11 +0900

    修正: バックグラウンドスクリプトの初期化処理を非同期関数でラップし、エラーハンドリングを追加。Service Workerのライフサイクルイベントをリッスンする機能を実装。

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,18 +1,41 @@
 import { BackgroundContainerProvider } from './provider/background_container_provider';
-import { MessageListener } from './services/message_listener';
-import { GestureActionHandler } from './services/gesture_action_handler';
-import { DragActionHandler } from './services/drag_action_handler';
+import { MessageListener, IGestureActionHandler, IDragActionHandler } from './services/message_listener';
 import Logger from '../common/logger/logger';
 
-Logger.debug('バックグラウンド初期設定開始');
+// 開発環境かどうかを確認
+Logger.info('バックグラウンドスクリプト開始');
 
-const containerProvider = new BackgroundContainerProvider();
-const container = containerProvider.getContainer();
+// 初期化処理を非同期関数でラップ
+async function initialize() {
+    try {
+        Logger.info('バックグラウンド初期設定開始');
 
-const gestureActionHandler = container.get(GestureActionHandler);
-const dragActionHandler = container.get(DragActionHandler);
-const messageListener = new MessageListener(gestureActionHandler, dragActionHandler);
+        const containerProvider = new BackgroundContainerProvider();
+        const container = containerProvider.getContainer();
 
-messageListener.listen();
+        const gestureActionHandler = container.get<IGestureActionHandler>('IGestureActionHandler');
+        const dragActionHandler = container.get<IDragActionHandler>('IDragActionHandler');
+        const messageListener = new MessageListener(gestureActionHandler, dragActionHandler);
 
-Logger.debug('バックグラウンド初期設定完了');
+        messageListener.listen();
+
+        Logger.info('バックグラウンド初期設定完了');
+    } catch (error) {
+        Logger.error('バックグラウンド初期化エラー', { error });
+    }
+}
+
+// Service Workerのライフサイクルイベントをリッスン
+chrome.runtime.onInstalled.addListener(() => {
+    Logger.info('拡張機能がインストール/アップデートされました');
+    initialize();
+});
+
+// Service Workerがアクティブになったときのイベント
+self.addEventListener('activate', (event) => {
+    Logger.info('Service Workerがアクティブになりました');
+    initialize();
+});
+
+// 初期化処理を実行
+initialize();
