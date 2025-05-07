cd8e3eb7aae3c12ba2b4a449bfd5d0bb8f333f6e
Nikawa <burorisuto3@gmail.com>
2025-05-07 21:02:15 +0900

    修正: GestureActionHandlerクラスにジェスチャアクションの処理を実装し、エラーハンドリングを追加。ユニットテストを新規作成し、アクションの実行とエラー時のログ出力を確認。

--- src/background/services/gesture_action_handler.ts
+++ src/background/services/gesture_action_handler.ts
@@ -1,10 +1,23 @@
 import { IGestureActionHandler } from './message_listener';
 import { GestureActionMessagePayload } from '../../content/services/message/message_types';
 import Logger from '../../common/logger/logger';
+import { injectable } from 'inversify';
+import { BackgroundContainerProvider } from '../provider/background_container_provider';
+import { GestureActionType } from './gesture_action/gesture_action_type';
+import { GestureActionFactory } from '../services/gesture_action/gesture_action_factory';
 
+@injectable()
 export class GestureActionHandler implements IGestureActionHandler {
     async handle(payload: GestureActionMessagePayload): Promise<void> {
         Logger.debug('ジェスチャアクション受信', { payload });
-        // TODO: アクション名ごとに処理を分岐し、タブ操作等を実装
+        try {
+            const container = new BackgroundContainerProvider().getContainer();
+            const actionType = payload.actionName as GestureActionType;
+            const action = GestureActionFactory.create(actionType, container);
+            await action.execute();
+            Logger.info('ジェスチャアクション実行完了', { actionName: payload.actionName });
+        } catch (error: any) {
+            Logger.error('ジェスチャアクション実行エラー', { error: error?.message, payload });
+        }
     }
 } 
