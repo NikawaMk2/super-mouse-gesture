662f86707857a851893e8fdd20ff0b4b75958315
Nikawa <burorisuto3@gmail.com>
2025-05-07 23:40:24 +0900

    修正: DragActionFactoryのインポートパスを、正しいサブディレクトリに更新。

--- src/background/services/drag_action_handler.ts
+++ src/background/services/drag_action_handler.ts
@@ -1,7 +1,7 @@
 import { IDragActionHandler } from './message_listener';
 import { DragActionMessagePayload } from '../../content/services/message/message_types';
 import Logger from '../../common/logger/logger';
-import { DragActionFactory } from './drag_action_factory';
+import { DragActionFactory } from './drag_action/drag_action_factory';
 import { BackgroundContainerProvider } from '../provider/background_container_provider';
 
 export class DragActionHandler implements IDragActionHandler {
