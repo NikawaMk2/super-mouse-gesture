f3f668eb788f33ce11abe132efd758e33f15d981
Nikawa <burorisuto3@gmail.com>
2025-05-07 23:32:26 +0900

    修正: 新しいタブやウィンドウ、タブの操作に関するジェスチャーアクションを追加し、依存性注入を通じてコンテナにバインド。ユーザーの操作性を向上させるための機能拡張。

--- src/background/provider/background_container_provider.ts
+++ src/background/provider/background_container_provider.ts
@@ -17,6 +17,23 @@ import { DownloadImageDragAction } from '../services/drag_action/download_image_
 import { SearchImageGoogleDragAction } from '../services/drag_action/search_image_google_drag_action';
 import { CopyImageUrlDragAction } from '../services/drag_action/copy_image_url_drag_action';
 import Logger from '../../common/logger/logger';
+import { NewTabGestureAction } from '../services/gesture_action/new_tab_gesture_action';
+import { CloseTabGestureAction } from '../services/gesture_action/close_tab_gesture_action';
+import { CloseTabToLeftGestureAction } from '../services/gesture_action/close_tab_to_left_gesture_action';
+import { CloseTabToRightGestureAction } from '../services/gesture_action/close_tab_to_right_gesture_action';
+import { NextTabGestureAction } from '../services/gesture_action/next_tab_gesture_action';
+import { PrevTabGestureAction } from '../services/gesture_action/prev_tab_gesture_action';
+import { ReopenClosedTabGestureAction } from '../services/gesture_action/reopen_closed_tab_gesture_action';
+import { DuplicateTabGestureAction } from '../services/gesture_action/duplicate_tab_gesture_action';
+import { PinTabGestureAction } from '../services/gesture_action/pin_tab_gesture_action';
+import { MuteTabGestureAction } from '../services/gesture_action/mute_tab_gesture_action';
+import { NewWindowGestureAction } from '../services/gesture_action/new_window_gesture_action';
+import { NewIncognitoWindowGestureAction } from '../services/gesture_action/new_incognito_window_gesture_action';
+import { CloseWindowGestureAction } from '../services/gesture_action/close_window_gesture_action';
+import { MinimizeWindowGestureAction } from '../services/gesture_action/minimize_window_gesture_action';
+import { MaximizeWindowGestureAction } from '../services/gesture_action/maximize_window_gesture_action';
+import { ToggleFullscreenGestureAction } from '../services/gesture_action/toggle_fullscreen_gesture_action';
+import { ChromeWindowOperator } from '../services/gesture_action/chrome_window_operator';
 
 export class BackgroundContainerProvider {
     private static container: Container | null = null;
@@ -73,6 +90,23 @@ export class BackgroundContainerProvider {
                 const clipboardService = ctx.container.get('IClipboardService');
                 return new CopyImageUrlDragAction(clipboardService);
             }).inSingletonScope();
+            container.bind(NewTabGestureAction).toSelf().inSingletonScope();
+            container.bind(CloseTabGestureAction).toSelf().inSingletonScope();
+            container.bind(CloseTabToLeftGestureAction).toSelf().inSingletonScope();
+            container.bind(CloseTabToRightGestureAction).toSelf().inSingletonScope();
+            container.bind(NextTabGestureAction).toSelf().inSingletonScope();
+            container.bind(PrevTabGestureAction).toSelf().inSingletonScope();
+            container.bind(ReopenClosedTabGestureAction).toSelf().inSingletonScope();
+            container.bind(DuplicateTabGestureAction).toSelf().inSingletonScope();
+            container.bind(PinTabGestureAction).toSelf().inSingletonScope();
+            container.bind(MuteTabGestureAction).toSelf().inSingletonScope();
+            container.bind(NewWindowGestureAction).toSelf().inSingletonScope();
+            container.bind(NewIncognitoWindowGestureAction).toSelf().inSingletonScope();
+            container.bind(CloseWindowGestureAction).toSelf().inSingletonScope();
+            container.bind(MinimizeWindowGestureAction).toSelf().inSingletonScope();
+            container.bind(MaximizeWindowGestureAction).toSelf().inSingletonScope();
+            container.bind(ToggleFullscreenGestureAction).toSelf().inSingletonScope();
+            container.bind('IWindowOperator').to(ChromeWindowOperator).inSingletonScope();
 
             return container;
         } catch (error) {
