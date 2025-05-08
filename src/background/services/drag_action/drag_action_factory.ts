796cbb92d05c100e5ebec0d651f93346b2b1ef6a
Nikawa <burorisuto3@gmail.com>
2025-05-08 22:54:21 +0900

    delete: DragActionFactoryから不要なドラッグアクション関連のインポートとテストケースを削除し、コードの整理を実施。

--- src/background/services/drag_action/drag_action_factory.ts
+++ src/background/services/drag_action/drag_action_factory.ts
@@ -2,15 +2,12 @@ import { DragAction } from './drag_action';
 import { SearchGoogleDragAction } from './search_google_drag_action';
 import { SearchBingDragAction } from './search_bing_drag_action';
 import { OpenAsUrlDragAction } from './open_as_url_drag_action';
-import { CopyTextDragAction } from './copy_text_drag_action';
 import { OpenInBackgroundTabDragAction } from './open_in_background_tab_drag_action';
 import { OpenInForegroundTabDragAction } from './open_in_foreground_tab_drag_action';
 import { DownloadLinkDragAction } from './download_link_drag_action';
-import { CopyLinkUrlDragAction } from './copy_link_url_drag_action';
 import { OpenImageInNewTabDragAction } from './open_image_in_new_tab_drag_action';
 import { DownloadImageDragAction } from './download_image_drag_action';
 import { SearchImageGoogleDragAction } from './search_image_google_drag_action';
-import { CopyImageUrlDragAction } from './copy_image_url_drag_action';
 import { Container } from 'inversify';
 import { DragActionType } from './drag_action_type';
 
@@ -23,24 +20,18 @@ export class DragActionFactory {
                 return container.get(SearchBingDragAction);
             case DragActionType.OPEN_AS_URL:
                 return container.get(OpenAsUrlDragAction);
-            case DragActionType.COPY_TEXT:
-                return container.get(CopyTextDragAction);
             case DragActionType.OPEN_IN_BACKGROUND_TAB:
                 return container.get(OpenInBackgroundTabDragAction);
             case DragActionType.OPEN_IN_FOREGROUND_TAB:
                 return container.get(OpenInForegroundTabDragAction);
             case DragActionType.DOWNLOAD_LINK:
                 return container.get(DownloadLinkDragAction);
-            case DragActionType.COPY_LINK_URL:
-                return container.get(CopyLinkUrlDragAction);
             case DragActionType.OPEN_IMAGE_IN_NEW_TAB:
                 return container.get(OpenImageInNewTabDragAction);
             case DragActionType.DOWNLOAD_IMAGE:
                 return container.get(DownloadImageDragAction);
             case DragActionType.SEARCH_IMAGE_GOOGLE:
                 return container.get(SearchImageGoogleDragAction);
-            case DragActionType.COPY_IMAGE_URL:
-                return container.get(CopyImageUrlDragAction);
             default:
                 throw new Error(`未対応のDragActionType: ${actionName}`);
         }
--- test/background/services/drag_action/drag_action_factory.test.ts
+++ test/background/services/drag_action/drag_action_factory.test.ts
@@ -5,18 +5,14 @@ import { DragActionType } from '../../../../src/background/services/drag_action/
 import { SearchGoogleDragAction } from '../../../../src/background/services/drag_action/search_google_drag_action';
 import { SearchBingDragAction } from '../../../../src/background/services/drag_action/search_bing_drag_action';
 import { OpenAsUrlDragAction } from '../../../../src/background/services/drag_action/open_as_url_drag_action';
-import { CopyTextDragAction } from '../../../../src/background/services/drag_action/copy_text_drag_action';
 import { OpenInBackgroundTabDragAction } from '../../../../src/background/services/drag_action/open_in_background_tab_drag_action';
 import { OpenInForegroundTabDragAction } from '../../../../src/background/services/drag_action/open_in_foreground_tab_drag_action';
 import { DownloadLinkDragAction } from '../../../../src/background/services/drag_action/download_link_drag_action';
-import { CopyLinkUrlDragAction } from '../../../../src/background/services/drag_action/copy_link_url_drag_action';
 import { OpenImageInNewTabDragAction } from '../../../../src/background/services/drag_action/open_image_in_new_tab_drag_action';
 import { DownloadImageDragAction } from '../../../../src/background/services/drag_action/download_image_drag_action';
 import { ITabOperator } from '../../../../src/common/provider/tab_operator';
-import { IClipboardService } from '../../../../src/background/services/clipboard/clipboard_service_interface';
 import { IDownloadService } from '../../../../src/background/services/download_service_interface';
 import { SearchImageGoogleDragAction } from '../../../../src/background/services/drag_action/search_image_google_drag_action';
-import { CopyImageUrlDragAction } from '../../../../src/background/services/drag_action/copy_image_url_drag_action';
 
 describe('DragActionFactory', () => {
     let container: Container;
@@ -36,9 +32,6 @@ describe('DragActionFactory', () => {
             duplicateActiveTab: jest.fn(),
             reopenClosedTab: jest.fn(),
         };
-        const clipboardServiceMock: IClipboardService = {
-            writeText: jest.fn(),
-        };
         const downloadServiceMock: IDownloadService = {
             download: jest.fn(),
         };
@@ -46,15 +39,12 @@ describe('DragActionFactory', () => {
         container.bind(SearchGoogleDragAction).toSelf();
         container.bind(SearchBingDragAction).toSelf();
         container.bind(OpenAsUrlDragAction).toDynamicValue(() => new OpenAsUrlDragAction(tabOperatorMock));
-        container.bind(CopyTextDragAction).toDynamicValue(() => new CopyTextDragAction(clipboardServiceMock));
         container.bind(OpenInBackgroundTabDragAction).toDynamicValue(() => new OpenInBackgroundTabDragAction(tabOperatorMock));
         container.bind(OpenInForegroundTabDragAction).toDynamicValue(() => new OpenInForegroundTabDragAction(tabOperatorMock));
         container.bind(DownloadLinkDragAction).toDynamicValue(() => new DownloadLinkDragAction(downloadServiceMock));
-        container.bind(CopyLinkUrlDragAction).toSelf();
         container.bind(OpenImageInNewTabDragAction).toDynamicValue(() => new OpenImageInNewTabDragAction(tabOperatorMock));
         container.bind(DownloadImageDragAction).toDynamicValue(() => new DownloadImageDragAction(downloadServiceMock));
         container.bind(SearchImageGoogleDragAction).toDynamicValue(() => new SearchImageGoogleDragAction(tabOperatorMock));
-        container.bind(CopyImageUrlDragAction).toDynamicValue(() => new CopyImageUrlDragAction(clipboardServiceMock));
     });
 
     it('SEARCH_GOOGLEでSearchGoogleDragActionを返す', () => {
@@ -69,10 +59,6 @@ describe('DragActionFactory', () => {
         const action = DragActionFactory.create(DragActionType.OPEN_AS_URL, container);
         expect(action).toBeInstanceOf(OpenAsUrlDragAction);
     });
-    it('COPY_TEXTでCopyTextDragActionを返す', () => {
-        const action = DragActionFactory.create(DragActionType.COPY_TEXT, container);
-        expect(action).toBeInstanceOf(CopyTextDragAction);
-    });
     it('OPEN_IN_BACKGROUND_TABでOpenInBackgroundTabDragActionを返す', () => {
         const action = DragActionFactory.create(DragActionType.OPEN_IN_BACKGROUND_TAB, container);
         expect(action).toBeInstanceOf(OpenInBackgroundTabDragAction);
@@ -85,10 +71,6 @@ describe('DragActionFactory', () => {
         const action = DragActionFactory.create(DragActionType.DOWNLOAD_LINK, container);
         expect(action).toBeInstanceOf(DownloadLinkDragAction);
     });
-    it('COPY_LINK_URLでCopyLinkUrlDragActionを返す', () => {
-        const action = DragActionFactory.create(DragActionType.COPY_LINK_URL, container);
-        expect(action).toBeInstanceOf(CopyLinkUrlDragAction);
-    });
     it('OPEN_IMAGE_IN_NEW_TABでOpenImageInNewTabDragActionを返す', () => {
         const action = DragActionFactory.create(DragActionType.OPEN_IMAGE_IN_NEW_TAB, container);
         expect(action).toBeInstanceOf(OpenImageInNewTabDragAction);
@@ -100,10 +82,6 @@ describe('DragActionFactory', () => {
     it('SEARCH_IMAGE_GOOGLEでSearchImageGoogleDragActionを返す', () => {
         const action = DragActionFactory.create(DragActionType.SEARCH_IMAGE_GOOGLE, container);
         expect(action).toBeInstanceOf(SearchImageGoogleDragAction);
-    });
-    it('COPY_IMAGE_URLでCopyImageUrlDragActionを返す', () => {
-        const action = DragActionFactory.create(DragActionType.COPY_IMAGE_URL, container);
-        expect(action).toBeInstanceOf(CopyImageUrlDragAction);
     });
     it('未対応のDragActionTypeの場合は例外を投げる', () => {
         expect(() => {
