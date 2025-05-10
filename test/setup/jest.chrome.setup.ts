e6b703b56f24f58441c946ab9a25998358da9dcb
Nikawa <burorisuto3@gmail.com>
2025-05-10 22:36:57 +0900

    update: jest.chrome.setup.tsにおいて、chrome.runtimeのsendMessageおよびonMessageのモックを強化し、メッセージハンドラの追加・削除機能を実装。windowsオブジェクトのメソッドも追加し、テストの拡張性を向上。

--- test/setup/jest.chrome.setup.ts
+++ test/setup/jest.chrome.setup.ts
@@ -1,13 +1,23 @@
+let onMessageHandler: ((message: any, sender: any, sendResponse: (response: any) => void) => void) | null = null;
 (global as any).chrome = {
   runtime: {
-    sendMessage: jest.fn((message, callback) => {
-      if (callback) {
+    sendMessage: jest.fn((message: any, callback: (response: any) => void) => {
+      if (onMessageHandler) {
+        // sender, sendResponseは最低限のモック
+        onMessageHandler(message, {}, (response: any) => {
+          if (callback) callback(response);
+        });
+      } else if (callback) {
         callback({});
       }
     }),
     onMessage: {
-      addListener: jest.fn(),
-      removeListener: jest.fn(),
+      addListener: jest.fn((handler: (message: any, sender: any, sendResponse: (response: any) => void) => void) => {
+        onMessageHandler = handler;
+      }),
+      removeListener: jest.fn(() => {
+        onMessageHandler = null;
+      }),
     },
     lastError: null,
   },
@@ -16,6 +26,12 @@
     query: jest.fn(),
     update: jest.fn(),
     duplicate: jest.fn(),
+    remove: jest.fn(),
+  },
+  windows: {
+    create: jest.fn(),
+    getCurrent: jest.fn(),
+    update: jest.fn(),
     remove: jest.fn(),
   },
 }; 
