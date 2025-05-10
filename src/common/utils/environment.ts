e2e6c2535515cfbd5f99e96062418ce42c6c4b1b
Nikawa <burorisuto3@gmail.com>
2025-05-10 17:49:58 +0900

    update: EnvironmentクラスにisProductionメソッドとisTestWithDebugLogメソッドを追加し、環境判定のロジックを強化。

--- src/common/utils/environment.ts
+++ src/common/utils/environment.ts
@@ -1,5 +1,9 @@
 export default class Environment {
-    static isDevelopment(): boolean {
-        return process.env.NODE_ENV === 'development';
+    static isProduction(): boolean {
+        return process.env.NODE_ENV === 'production';
+    }
+
+    static isTestWithDebugLog(): boolean {
+        return process.env.NODE_ENV === 'test' && process.env.DEBUG_LOG === 'true';
     }
 }
