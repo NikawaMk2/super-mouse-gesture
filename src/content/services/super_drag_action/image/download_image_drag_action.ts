58f0605ed8efb22fc6d6f0ba28ac101e31e5548e
Nikawa <burorisuto3@gmail.com>
2025-05-20 22:39:41 +0900

    add: DownloadImageDragActionクラスにIDragActionMessageSenderを導入し、メッセージ送信の依存性を注入可能に。統合テストを追加し、正常系と異常系の動作を検証。

--- src/content/services/super_drag_action/image/download_image_drag_action.ts
+++ src/content/services/super_drag_action/image/download_image_drag_action.ts
@@ -1,10 +1,14 @@
 import { SuperDragAction } from '../super_drag_action';
 import Logger from '../../../../common/logger/logger';
-import { ChromeMessageSender } from '../../message/message_sender';
+import { ChromeMessageSender, IDragActionMessageSender } from '../../message/message_sender';
 import { DragType } from '../../../models/drag_type';
 import { Direction } from '../../../models/direction';
 
 export class DownloadImageDragAction implements SuperDragAction {
+    private sender: IDragActionMessageSender;
+    constructor(sender: IDragActionMessageSender = new ChromeMessageSender()) {
+        this.sender = sender;
+    }
     async execute(options: {
         type: DragType;
         direction: Direction;
@@ -18,8 +22,7 @@ export class DownloadImageDragAction implements SuperDragAction {
             Logger.warn('ダウンロードする画像URLが指定されていません', { options });
             return;
         }
-        const sender = new ChromeMessageSender();
-        await sender.sendDragAction({
+        await this.sender.sendDragAction({
             ...options,
             type: DragType.IMAGE,
             direction: options.direction,
