15c4a023965ec87408d9a8f8c36be350bc90bbc9
Nikawa <burorisuto3@gmail.com>
2025-05-24 21:46:17 +0900

    update: DragContextクラスに子要素および親要素の<a>タグを考慮したドラッグタイプ判定ロジックを追加し、選択値取得の精度を向上。コードの可読性を改善。

--- src/content/handlers/drag_context.ts
+++ src/content/handlers/drag_context.ts
@@ -3,6 +3,7 @@ import { DragType } from "../models/drag_type";
 export class DragContext {
     public static readonly TAGNAME_A = 'A';
     public static readonly TAGNAME_IMG = 'IMG';
+    public static readonly CSS_SELECTOR_A = 'a';
 
     constructor(
         public readonly dragType: DragType,
@@ -14,13 +15,27 @@ export class DragContext {
         const selection = window.getSelection();
         if (selection && selection.toString()) {
             return new DragContext(DragType.TEXT, selection.toString());
-        } else if ((e.target as HTMLElement).tagName === DragContext.TAGNAME_A) {
-            return new DragContext(DragType.LINK, (e.target as HTMLAnchorElement).href);
-        } else if ((e.target as HTMLElement).tagName === DragContext.TAGNAME_IMG) {
-            return new DragContext(DragType.IMAGE, (e.target as HTMLImageElement).src);
-        } else {
-            return DragContext.default();
         }
+
+        const target = e.target as HTMLElement;
+        if (target.tagName === DragContext.TAGNAME_A) {
+            return new DragContext(DragType.LINK, (target as HTMLAnchorElement).href);
+        } else if (target.tagName === DragContext.TAGNAME_IMG) {
+            return new DragContext(DragType.IMAGE, (target as HTMLImageElement).src);
+        }
+
+        // 子要素に<a>タグがあるかチェック
+        const child = target.querySelector(DragContext.CSS_SELECTOR_A);
+        if (child) {
+            return new DragContext(DragType.LINK, (child as HTMLAnchorElement).href);
+        }
+        // 親要素に<a>タグがあるかチェック
+        const parent = target.closest(DragContext.CSS_SELECTOR_A);
+        if (parent) {
+            return new DragContext(DragType.LINK, (parent as HTMLAnchorElement).href);
+        }
+
+        return DragContext.default();
     }
 
     public static default(): DragContext {
