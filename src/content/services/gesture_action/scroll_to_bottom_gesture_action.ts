cf624686194ea071225a358cae627c9860754003
Nikawa <burorisuto3@gmail.com>
2025-06-07 15:38:19 +0900

    update: スクロールアクションの挙動を変更し、スムーズなスクロールから自動スクロールに切り替え。関連するテストケースも修正。

--- src/content/services/gesture_action/scroll_to_bottom_gesture_action.ts
+++ src/content/services/gesture_action/scroll_to_bottom_gesture_action.ts
@@ -4,6 +4,6 @@ import Logger from '../../../common/logger/logger';
 export class ScrollToBottomGestureAction implements GestureAction {
     execute(): void {
         Logger.debug('ScrollToBottomGestureAction: execute() が呼び出されました');
-        window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+        window.scrollTo({ top: document.body.scrollHeight, behavior: 'auto' });
     }
 } 
